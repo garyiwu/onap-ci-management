986ac7473a700f6bbb9f7f8c64438a5cc3f9192b
Gary Wu <gary.wu@gmail.com>
2017-11-08 23:47:40 +0000

    Additional nexus replacements

--- jjb/include-replace-nexus.sh
+++ jjb/include-replace-nexus.sh
@@ -1,4 +1,5 @@
 #!/bin/bash -x
 
-perl -p -i -e 's|https://nexus.onap.org|http://localhost:8081/nexus|g' `find ./ -name pom.xml`
+perl -p -i -e 's|https://nexus.onap.org|http://localhost:8081/nexus|g' $(find ./ -name pom.xml)
+perl -p -i -e 's|<nexusUrl>.*</nexusUrl>|<nexusUrl>http://localhost:8081/nexus</nexusUrl>|g' $(find ./ -name pom.xml)
 git diff
