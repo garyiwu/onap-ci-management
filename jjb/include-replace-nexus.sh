1c78832a1f4d25861f8e1655977ca44ae918a52d
Gary Wu <gary.wu@gmail.com>
2017-11-13 19:30:14 +0000

    Change nexus 3 url to localhost

--- jjb/include-replace-nexus.sh
+++ jjb/include-replace-nexus.sh
@@ -2,4 +2,5 @@
 
 perl -p -i -e 's|https://nexus.onap.org|http://localhost:8081/nexus|g' $(find ./ -name pom.xml)
 perl -p -i -e 's|<nexusUrl>.*</nexusUrl>|<nexusUrl>http://localhost:8081/nexus</nexusUrl>|g' $(find ./ -name pom.xml)
+perl -p -i -e 's|nexus3.onap.org|localhost|g' $(find ./ -name pom.xml)
 git diff
--- jjb/testsuite/testsuite-docker.sh
+++ jjb/testsuite/testsuite-docker.sh
@@ -3,7 +3,7 @@
 echo '============== CALLING SCRIPT TO CREATE DOCKER IMAGES ================='
 cp $WORKSPACE/docker/* .
 docker -D build -t openecomp/testsuite .
-export REPO="nexus3.onap.org:10003"
+export REPO="localhost:10003"
 for tag in $tags
 do
   docker tag openecomp/testsuite:latest $REPO/openecomp/testsuite:$tag
